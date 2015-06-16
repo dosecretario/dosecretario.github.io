28b5a2487d70ffd4a7c9d5aab1594db1f146940f
Felipe Tavares <felipe.oltavares@gmail.com>
2015-06-16 14:13:02 -0300

    Adicionada informação sobre o horário atual.

--- js/doSecretário.js
+++ js/doSecretário.js
@@ -1,5 +1,15 @@
 // Adiciona .overflown() ao jQuery
 $.fn.overflown=function(){var e=this[0];return e.scrollHeight>e.clientHeight||e.scrollWidth>e.clientWidth}
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length == 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
 
 function doSecretário (url) {
   this.calendario = null;
@@ -11,12 +21,12 @@ function doSecretário (url) {
   this.criaDOM_nome = function () {
     var dom = $("<div class=\"nome\">"+this.calendario.nome+"</div>");
 
-    $(document.body).append(dom);
+    $("#content").append(dom);
 
     this.dom.nome = dom;
   }
 
-  this.criaDOM_horários = function (domDia, dados) {
+  this.criaDOM_horários = function (domDia, dados, selecionado) {
     var soma = 0;
 
     for (var i in dados) {
@@ -24,11 +34,16 @@ function doSecretário (url) {
       soma += horario[0];
     }
 
+    var horario_atual = 0;
+    var horario_antigo = 0;
     for (var i in dados) {
       var horario = dados[i];
 
       var num = horario[0];
       var nome = horario[1];
+
+      horario_antigo = horario_atual;
+      horario_atual += num;
 
       nome = nome.replace (/#\{([^#]*)\|([^#]*)\}/g, function (a, b, c) {
         return "<br><span onclick=\"showInfoFor('"+c+"')\" class=\"atividade\">"+""+b+"</span>";
@@ -36,19 +51,30 @@ function doSecretário (url) {
 
       var dom = $("<div class=\"horario\">"+nome+"</div>");
 
+      var date = new Date();
+      var minutes = date.getHours()*60+date.getMinutes()-7*60;
+
+      if (minutes > 6*60)
+        minutes -= 60;
+
       domDia.append(dom);
+
+
+      if ((Math.floor(minutes/50) <= horario_atual &&
+           Math.floor(minutes/50) > horario_antigo) && selecionado) {
+        dom.css("background", "#ccf");
+        dom.css("box-shadow", "0 0 2px 0 black inset");
+      }
 
       this.horarios.push(dom);
       this.alturas.push(num/soma);
     }
   }
 
-  this.criaDOM_dia = function (nome) {
+  this.criaDOM_dia = function (nome, destaca) {
     var dom = $("<div class=\"dia\"></div>");
     var dom_nome = $("<div class=\"titulo\">"+nome+"</div>");
     var tamanho_dia = 100/this.calendario.dias;
-
-    console.log ("tamanho_dia: "+tamanho_dia);
 
     dom.css("width", Math.floor(tamanho_dia)+"%");
     dom_nome.css("width", Math.floor(tamanho_dia)+"%");
@@ -56,7 +82,11 @@ function doSecretário (url) {
     this.dom.barra.append(dom_nome);
     this.dom.calendario.append(dom);
 
-    this.criaDOM_horários(dom, this.calendario[nome]);
+    if (destaca) {
+      dom_nome.css("box-shadow", "0 0 2px 0 black inset");
+    }
+
+    this.criaDOM_horários(dom, this.calendario[nome], destaca);
 
     this.dom.dias.push(dom);
   }
@@ -66,17 +96,20 @@ function doSecretário (url) {
     this.dom.barra = $("<div class=\"barra\"></div>")
     this.dom.calendario = $("<div class=\"calendario\"></div>")
 
-    $(document.body).append(this.dom.barra);
-    $(document.body).append(this.dom.calendario);
+    $("#content").append(this.dom.barra);
+    $("#content").append(this.dom.calendario);
 
-    if (this.dom.calendario.height() < 24) {
-    }
-
+    var ndia = 0;
     for (var chave in this.calendario) {
       if (chave != "nome" &&
           chave != "dias") {
-        this.criaDOM_dia(chave);
+          if ((new Date()).getDay() == ndia)
+            this.criaDOM_dia(chave, true);
+          else
+            this.criaDOM_dia(chave, false);
       }
+
+      ndia++;
     }
 
     this.dom.barra.append($("<div class=\"clear\"></div>"));
@@ -84,8 +117,8 @@ function doSecretário (url) {
   }
 
   this.criaDOM = function () {
-    // TODO: Verificar se já existe um DOM e apagar
-
+    if (this.dom && this.dom.calendario)
+      this.dom.calendario.remove();
     this.dom = {};
 
     if (this.calendario.nome) {
@@ -95,9 +128,9 @@ function doSecretário (url) {
 
     this.criaDOM_dias();
 
-    //$(document.body).append($("<div class=\"rodape\">"+""+"</div>"));
-    $(document.body).append($("<div class=\"info\"><div class=\"info-texto\"></div></div>"));
-    $(document.body).append($("<div class=\"fechar\"></div>"));
+    //$("#content").append($("<div class=\"rodape\">"+""+"</div>"));
+    $("#content").append($("<div class=\"info\"><div class=\"info-texto\"></div></div>"));
+    $("#content").append($("<div class=\"fechar\"></div>"));
   }
 
   this.semOverflow = function () {
